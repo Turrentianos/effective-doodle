f6e8669843bce8d8b0b9403062e23414abd52694
Lucianos Lionakis <luckianos@hotmail.com>
2023-01-04 19:05:10 +0100

    Checking for swap chain support

--- main.cpp
+++ main.cpp
@@ -16,6 +16,10 @@ const std::vector<const char*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
 };
 
+const std::vector<const char*> deviceExtensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
 #ifdef NDEBUG
     const bool enableValidationLayers = false;
 #else
@@ -199,6 +203,23 @@ private:
         return true;
     }
 
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device) {
+        uint32_t extensionCount;
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount,
+            nullptr);
+
+        std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount,
+            availableExtensions.data());
+        
+        std::set<std::string> requiredExtensions(deviceExtensions.begin(),
+            deviceExtensions.end());
+        for (const auto& extension : availableExtensions) {
+            requiredExtensions.erase(extension.extensionName);
+        }
+        return requiredExtensions.empty();
+    }
+    
     void createLogicalDevice() {
         QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
@@ -244,8 +265,11 @@ private:
         {
             throw std::runtime_error("failed to create logical device!");
         }
-        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
-        vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
+
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0,
+            &graphicsQueue);
+        vkGetDeviceQueue(device, indices.presentFamily.value(), 0,
+            &presentQueue);
     }
 
     void pickPhysicalDevice() {
@@ -271,7 +295,10 @@ private:
 
     bool isDeviceSuitable(VkPhysicalDevice device) {
         QueueFamilyIndices indices = findQueueFamilies(device);
-        return indices.isComplete();
+
+        bool extensionsSupported = checkDeviceExtensionSupport(device);
+
+        return indices.isComplete() && extensionsSupported;
     }
 
     struct QueueFamilyIndices {
